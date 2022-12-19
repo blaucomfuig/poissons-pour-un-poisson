b69bf92d792e02b89aaef5c0547c701864fef13d
scorcuera <scorcuera6@gmail.com>
2022-12-19 08:08:17 +0100

    refactored API request

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,24 @@
 const INFO_CONTAINER = document.querySelector(".header__info");
 
-function getPececito() {
-    const getPececitoAsync = async() => {
-        try {
-            const res = await fetch('https://www.fishwatch.gov/api/species')
-            const data = await res.json()
+async function getFish() {
+    try {
+        const res = await fetch('https://www.fishwatch.gov/api/species');
+        const data = await res.json();
+        return data;
 
-            imagenPececito = data.map(element => element["Species Illustration Photo"].src)
-            nombrePececito = data.map(element => element["Species Name"])
-
-            const randomPececito = Math.floor(Math.random() * imagenPececito.length);
-            INFO_CONTAINER.innerHTML = `<div class="d-flex flex-column align-items-center justify-content-center"><h1 class="header__info-name">${nombrePececito[randomPececito]}</h1>
-            <div class="header__info-image d-flex align-items-center justify-content-center">
-            <img src="${imagenPececito[randomPececito]}" class=" justify-content-center" alt="... ">
-            </div>
-            </div>`
-
-
-        } catch (error) {
-            console.log(error)
-        }
+    } catch (error) {
+        console.log(error)
     }
-    getPececitoAsync()
+}
 
+async function printFish() {
+    const fishesData = await getFish();
+    let fishImages = fishesData.map(element => element["Species Illustration Photo"].src)
+    let fishNames = fishesData.map(element => element["Species Name"])
+    const randomFish = Math.floor(Math.random() * fishImages.length);
+    INFO_CONTAINER.innerHTML = `<div class="d-flex flex-column align-items-center justify-content-center"><h1 class="header__info-name">${fishNames[randomFish]}</h1>
+    <div class="header__info-image d-flex align-items-center justify-content-center">
+    <img src="${fishImages[randomFish]}" class=" justify-content-center" alt="... ">
+    </div>
+    </div>`
 }
